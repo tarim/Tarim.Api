ff045fad0e816d84e92d10351d01c42dcece0fc5
Nur Karluk <ns.karluk@gmail.com>
2020-03-14 16:16:15 -0400

    added service project

---
+++ Tarim.Api.Infrastructure.Service/NameRepository.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NameRepository.cs" company="Tarim Lab">
+//   Copyright (c) Tarim Lab. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tarim.Api.Infrastructure.Common;
+using Tarim.Api.Infrastructure.DataProvider;
+using Tarim.Api.Infrastructure.Interface;
+using Tarim.Api.Infrastructure.Model.names;
+using Microsoft.Extensions.Logging;
+
+namespace Tarim.Api.Infrastructure.Service
+{
+    public class NameRepository : BaseRepository, INameRepository
+    {
+        private readonly ILogger<NameRepository> logger;
+        public NameRepository(IConnection connection,ILogger<NameRepository> log) : base(connection)
+        {
+            logger = log;
+        }
+
+        public async Task<Result<IList<UyghurName>>> GetUyghurName()
+        {
+            var uyghurNames = new Result<IList<UyghurName>> { Object = new List<UyghurName>() };
+            await GetResultAsync("GET_UYGHUR_NAMES",
+                rdReader =>
+                {
+                    uyghurNames.Object.Read(rdReader);
+                    return uyghurNames;
+                });
+            return uyghurNames;
+            
+        }
+
+        public Task<Result<UyghurName>> GetUyghurName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Result<UyghurName>> UpdateUyghurName(UyghurName uyghurName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+      public Task<Result<UyghurName>> AddUyghurName(UyghurName uyghurName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Result<int>> DeleteUyghurName(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Tarim.Api.Infrastructure.Service/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tarim.Api.Infrastructure.Common;
+using Tarim.Api.Infrastructure.DataProvider;
+using Tarim.Api.Infrastructure.Interface;
+using Tarim.Api.Infrastructure.Model;
+using Microsoft.Extensions.Logging;
+
+namespace Tarim.Api.Infrastructure.Service
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        private readonly ILogger<UserRepository> logger;
+        public UserRepository(IConnection connection, ILogger<UserRepository> log) : base(connection)
+        {
+            logger = log;
+        }
+
+        public Task<Result<User>> FindUser(string userEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
